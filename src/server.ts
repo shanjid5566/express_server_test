6806fb85f75db8b77583ed32b27041f2c9d863c1
shanjid5566 <shanjidahmed66@gmail.com>
2025-12-19 14:31:04 +0600

    add db config

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,12 @@
 import express, { Request, Response } from "express";
-import { Pool } from "pg";
+
 import config from "./config";
+import initDB, { pool } from "./config/db";
 
 const app = express();
 const port = config.port;
 
-const pool = new Pool({
-  connectionString: `${config.connection_str}`,
-});
 
-const initDB = async () => {
-  await pool.query(`
-        CREATE TABLE IF NOT EXISTS users(
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(150) UNIQUE NOT NULL,
-        age INT,
-        phone VARCHAR(15),
-        address TEXT,
-        create_at TIMESTAMP DEFAULT NOW(),
-        updste_AT TIMESTAMP DEFAULT NOW()
-        )
-        `);
-  await pool.query(`
-            CREATE TABLE IF NOT EXISTS todos(
-            id SERIAL PRIMARY KEY,
-            user_id INT REFERENCES users(id) ON DELETE CASCADE,
-            title VARCHAR(200) NOT NULL,
-            description TEXT,
-            completed BOOLEAN DEFAULT false,
-            due_date DATE,
-            created_at TIMESTAMP DEFAULT NOW(),
-            updated_at TIMESTAMP DEFAULT NOW()
-            )
-            `);
-};
 initDB();
 app.use(express.json());
 app.use(express.urlencoded());

146dbb96fcae67b31a962dc3735bf15d2450ab1c
shanjid5566 <shanjidahmed66@gmail.com>
2025-12-20 11:12:39 +0600

    express type added

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,13 +1,18 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import config from "../config";
 
 const auth = () => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
-    const decoded = jwt.verify(token as string, config.user_secret_key);
-    console.log(decoded);
-    next();
+    try {
+      const token = req.headers.authorization;
+      const decoded = jwt.verify(token as string, config.user_secret_key);
+      console.log(decoded);
+      req.user = decoded as JwtPayload;
+      next();
+    } catch (error) {
+      res.status(401).json({ message: "Unauthorized" });
+    }
   };
 };
 export default auth;

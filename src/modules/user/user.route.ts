ad5972cf6ce8434590d88690bade96a675e9510b
shanjid5566 <shanjidahmed66@gmail.com>
2025-12-20 15:42:24 +0600

    server and app

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,14 +1,15 @@
 import exprss, { Request, Response } from "express";
 import { pool } from "../../config/db";
 import { userController } from "./user.controller";
+import auth from "../../middleware/auth";
 
 const route = exprss.Router();
 
-route.get("/", userController.getUser);
+route.get("/", auth("admin"), userController.getUser);
 
 route.post("/", userController.createUser);
 
-route.get("/:id", userController.getSingleUser);
+route.get("/:id", auth("admin", "user"), userController.getSingleUser);
 route.put("/:id", userController.updateUser);
 
 route.delete("/:id", userController.deleteUser);
--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,7 @@
-import express, { Request, Response } from "express";
-
+import app from "./app";
 import config from "./config";
-import initDB, { pool } from "./config/db";
-import logger from "./middleware/logger";
-import { userRoute } from "./modules/user/user.route";
-import { todoRoute } from "./modules/todo/todo.route";
-import { authRoutes } from "./modules/auth/auth.route";
-import auth from "./middleware/auth";
-
-const app = express();
 const port = config.port;
 
-initDB();
-app.use(express.json());
-app.use(express.urlencoded());
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!.....");
-});
-
-// Users  Route
-app.use("/users", logger, auth(), userRoute);
-
-
-// Create To for indivisual user
-
-app.use("/todos", todoRoute);
-
-
-// Auth routes
-app.use("/auth",authRoutes)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
